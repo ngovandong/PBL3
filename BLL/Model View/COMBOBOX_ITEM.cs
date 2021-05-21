6613bf9dde7e5d632ff142d198645c09871cf18e
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-05-21 15:20:44 +0700

    Add COMBOBOX_ITEM

---
+++ BLL/Model View/COMBOBOX_ITEM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Model_View
+{
+    public class COMBOBOX_ITEM
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+}

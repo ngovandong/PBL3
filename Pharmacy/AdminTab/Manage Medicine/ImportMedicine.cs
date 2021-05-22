846d7bf309448f3d63396e58fb80d4dcfa41fa8d
Dat Nguyen <datquoc.292001@gmail.com>
2021-05-22 11:15:45 +0700

    importmedicine

--- Pharmacy/AdminTab/Manage Medicine/ImportMedicine.cs
+++ Pharmacy/AdminTab/Manage Medicine/ImportMedicine.cs
@@ -22,5 +22,17 @@ namespace Pharmacy.AdminTab.Manage_Medicine
             AddStock stock = new AddStock();
             stock.ShowDialog();
         }
+
+        private void buttonDetail_Click(object sender, EventArgs e)
+        {
+            StockDetail stockDetail = new StockDetail();
+            stockDetail.ShowDialog();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            StockDetail stockDetail = new StockDetail();
+            stockDetail.ShowDialog();
+        }
     }
 }

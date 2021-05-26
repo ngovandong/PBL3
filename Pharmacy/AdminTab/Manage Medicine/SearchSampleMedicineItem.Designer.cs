562533489df37a9b859b0ba24493a4fbab1cf79c
minh duc <69248042+beobiebom@users.noreply.github.com>
2021-05-26 18:44:32 +0700

    Edit sample medicine

---
+++ Pharmacy/AdminTab/Manage Medicine/SearchSampleMedicineItem.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Pharmacy.AdminTab.Manage_Medicine
+{
+    partial class SearchSampleMedicineItem
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.lbUnit = new System.Windows.Forms.Label();
+            this.lbPrice = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.Controls.Add(this.lbUnit);
+            this.guna2Panel1.Controls.Add(this.lbPrice);
+            this.guna2Panel1.Controls.Add(this.label3);
+            this.guna2Panel1.Controls.Add(this.label2);
+            this.guna2Panel1.Controls.Add(this.lbName);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(374, 51);
+            this.guna2Panel1.TabIndex = 0;
+            this.guna2Panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseClick);
+            // 
+            // lbUnit
+            // 
+            this.lbUnit.AutoSize = true;
+            this.lbUnit.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbUnit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.lbUnit.Location = new System.Drawing.Point(296, 31);
+            this.lbUnit.Name = "lbUnit";
+            this.lbUnit.Size = new System.Drawing.Size(35, 17);
+            this.lbUnit.TabIndex = 9;
+            this.lbUnit.Text = "Viên";
+            this.lbUnit.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseClick);
+            // 
+            // lbPrice
+            // 
+            this.lbPrice.AutoSize = true;
+            this.lbPrice.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbPrice.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.lbPrice.Location = new System.Drawing.Point(112, 30);
+            this.lbPrice.Name = "lbPrice";
+            this.lbPrice.Size = new System.Drawing.Size(48, 17);
+            this.lbPrice.TabIndex = 8;
+            this.lbPrice.Text = "50000";
+            this.lbPrice.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.label3.Location = new System.Drawing.Point(234, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 7;
+            this.label3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.label2.Location = new System.Drawing.Point(44, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Giá bán:";
+            this.label2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseClick);
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Times New Roman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(18)))), ((int)(((byte)(53)))));
+            this.lbName.Location = new System.Drawing.Point(43, 3);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(160, 23);
+            this.lbName.TabIndex = 5;
+            this.lbName.Text = "Thuốc ho Prospan";
+            this.lbName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.guna2Panel1_MouseClick);
+            // 
+            // SearchMedicineItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "SearchMedicineItem";
+            this.Size = new System.Drawing.Size(374, 51);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label lbUnit;
+        private System.Windows.Forms.Label lbPrice;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbName;
+    }
+}
